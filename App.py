7e746521737a89a1860304ddcb075a5ecc47ca9f
RSBC2 <samiabdullah2304@yahoo.com>
2023-08-07 03:43:18 -0700

    Initial commit

---
+++ App.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify, render_template
+import openai
+
+# Load the OpenAI API key directly
+openai.api_key = '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'
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_message = data['message']
+    user_name = data.get('name', 'User')  # Get the user's name if provided, else default to 'User'
+
+    # Initial system message
+    system_message = 'You are an AI developed by OpenAI, functioning as a Tax Resolution Expert. You provide guidance on the IRS Fresh Start Program. Once you have the necessary information, you will inform the client that our Case Manager, Sami, will be calling them shortly. Sami can be reached directly at Sami@freshstarttaxco.com or 858-649-9433 during office hours (9 AM to 5 PM EST, Monday through Friday).'
+
+    # Process the user message with OpenAI
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[
+            {'role': 'system', 'content': system_message},
+            {'role': 'user', 'content': user_message},
+        ]
+    )
+
+    response_dict = dict(response)
+    chatbot_message = response_dict['choices'][0]['message']['content']
+
+    # Personalization
+    chatbot_message = chatbot_message.replace("User", user_name)
+
+    # Guided prompts
+    if "penalties" in user_message.lower():
+        chatbot_message += " Would you like to know more about how to reduce these penalties?"
+
+    return jsonify({'message': chatbot_message})
+
+if __name__ == '__main__':
+    app.run(debug=True)
